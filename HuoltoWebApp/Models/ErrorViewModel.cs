26332afc3b002339ec5f2e6aa4a8ea40be3b859a
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2024-03-26 16:05:36 +0200

    Add project files.

---
+++ HuoltoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HuoltoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HuoltoWebApp/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuoltoWebApp.Models
+{
+    public partial class Login
+    {
+        public int LoginId { get; set; }
+        public string Käyttäjätunnus { get; set; } = null!;
+        public string Salasana { get; set; } = null!;
+    }
+}
